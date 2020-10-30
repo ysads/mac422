3635b71578469bec0b186f079e2bfe92045ab263
Ygor Sad <ysads2010@gmail.com>
2020-10-30 12:44:54 -0300

    refactor: add movement

--- ep2/ep2.c
+++ ep2/ep2.c
@@ -10,7 +10,9 @@
 #define MAX_CICLISTAS_LARGADA 5
 #define FALSE 0
 #define TRUE 1
+#define INTERVAL_120MS 120000
 #define INTERVAL_60MS 60000
+#define INTERVAL_40MS 40000
 #define INTERVAL_20MS 20000
 
 int DEBUG = 1;
@@ -68,6 +70,7 @@ void print_pista() {
 
     pista = simulacao->pista;
 
+    printf("\n");
     for (i = 0; i < simulacao->d; i++) {
         fprintf(stderr, "%2d: \t", (i + 1));
         for (j = 0; j < MAX_CICLISTAS; j++) {
@@ -79,6 +82,84 @@ void print_pista() {
         }
         fprintf(stderr, "\n");
     }
+}
+
+
+/**
+ * Define quanto tempo deve passar antes entre duas mudanças de posição
+ * consecutivas.
+ */
+int intervalo(ciclista_t* ciclista) {
+    switch (ciclista->velocidade) {
+        case 30:
+            return INTERVAL_120MS;
+            break;
+
+        case 60:
+            return INTERVAL_60MS;
+            break;
+    
+        case 90:
+            return INTERVAL_40MS;
+            break;
+    }
+    debug("Veloc. inválida: %d\n", ciclista->velocidade);
+    exit(1);
+}
+
+
+/**
+ * Tenta atualizar a posição de um ciclista, fazendo lock no mutex responsável
+ * pela posição de destino. Também altera o ciclista para que ele tenha as
+ * coordenadas da nova posição e a referência correta para a volta atual dele.
+ */
+void atualizar_posicao(ciclista_t* ciclista, int prox_i, int prox_j) {
+    posicao_t *prox_posicao, *posicao_atual;
+
+    posicao_atual = simulacao->pista[ciclista->i][ciclista->j];
+    prox_posicao = simulacao->pista[prox_i][prox_j];
+    
+    pthread_mutex_lock(&prox_posicao->mutex);   // Possível problema dos filósofos famintos aqui!
+    pthread_mutex_lock(&posicao_atual->mutex);  // Não está claro em que ordem deve haver o lock
+    
+    posicao_atual->ciclista = NULL;
+    prox_posicao->ciclista = ciclista;
+    
+    ciclista->i = prox_i;
+    ciclista->j = prox_j;
+    
+    pthread_mutex_unlock(&posicao_atual->mutex);
+    pthread_mutex_unlock(&prox_posicao->mutex);
+
+    if (prox_i == 0) {
+        ciclista->volta_atual++;
+        debug("%d => volta %d!\n", ciclista->id, ciclista->volta_atual);
+    }
+}
+
+
+/**
+ * Organiza o comportamento de um ciclista dentro da corrida, ie, definindo quando – e se –
+ * há mudança de velocidade, de posição, de status, etc.
+ */
+void* simular_ciclista(void* args) {
+    int prox_i, prox_j;
+    ciclista_t* ciclista = (ciclista_t*) args;
+
+    /**
+     * Espera até que o simulador autorize essa linha de ciclistas a se mover.
+     */
+    // pthread_cond_wait();
+
+    while (ciclista->volta_atual == 1) {
+        prox_i = (ciclista->i + 1) % simulacao->d;
+        prox_j = ciclista->j;      // aqui deve vir a decisão de ultrapassagem
+
+        atualizar_posicao(ciclista, prox_i, prox_j);
+        usleep(intervalo(ciclista));
+    }
+
+    return NULL;
 }
 
 
@@ -97,6 +178,8 @@ ciclista_t* init_ciclista(int id, int i, int j) {
     ciclista->i = i;
     ciclista->j = j;
     ciclista->volta_atual = 1;
+
+    pthread_create(&ciclista->thread, NULL, simular_ciclista, ciclista);
     
     return ciclista;
 }
@@ -138,16 +221,21 @@ posicao_t*** init_pista(int d) {
 
 
 simulacao_t* init_simulacao(int d, int n) {
-    simulacao_t* simulacao;
+    simulacao_t* sim;
 
-    simulacao = (simulacao_t*) malloc(sizeof(simulacao_t));
+    sim = (simulacao_t*) malloc(sizeof(simulacao_t));
     
-    simulacao->quebrados = (ciclista_t**) malloc(n * sizeof(ciclista_t*));
-    simulacao->pista = init_pista(d);
-    simulacao->d = d;
-    simulacao->n = n;
+    sim->ciclistas = (ciclista_t**) malloc(n * sizeof(ciclista_t*));
+    sim->pista = init_pista(d);
+    sim->d = d;
+    sim->n = n;
 
-    return simulacao;
+    int i;
+    for (i = 0; i < n; i++) {
+        sim->ciclistas[i] = NULL;
+    }
+
+    return sim;
 }
 
 
@@ -165,28 +253,14 @@ void free_simulacao(simulacao_t* simulacao, int d) {
             if (pista[i][j]->ciclista != NULL) {   // remover quando o ep estiver pronto, pq no final não vai haver ciclista na pista
                 free(pista[i][j]->ciclista);
             }
+            pthread_mutex_destroy(&pista[i][j]->mutex);
             free(pista[i][j]);
         }
         free(pista[i]);
     }
     free(simulacao->pista);
-    free(simulacao->quebrados);
+    free(simulacao->ciclistas);
     free(simulacao);
-}
-
-
-/**
- * Organiza o comportamento de um ciclista dentro da corrida, ie, definindo quando – e se –
- * há mudança de velocidade, de posição, de status, etc.
- */
-void* ciclista(void* args) {
-    // while () {
-        // se dá pra ultrapassar pela esquerda
-            // ultrapassa
-        // atualiza posição
-    // }
-
-    return NULL;
 }
 
 
@@ -196,11 +270,11 @@ void* ciclista(void* args) {
  * por um inteiro entre 0 e n-1 (inclusive).
  */
 void dar_largada(posicao_t*** pista, int d, int n) {
-    int i, j, id_ciclista, posicao_pista;
+    int i, j, id_ciclista, n_ciclistas;
     int* ids;
-    ciclista_t* info_ciclista;
+    ciclista_t* ciclista;
 
-    i = n;
+    n_ciclistas = n;
     ids = (int*) calloc(n, sizeof(int));
 
     /**
@@ -208,13 +282,13 @@ void dar_largada(posicao_t*** pista, int d, int n) {
      * o número de ciclistas na largada.
      */
     if (n % MAX_CICLISTAS_LARGADA) {
-        posicao_pista = ceil(n / MAX_CICLISTAS_LARGADA);
+        i = ceil(n / MAX_CICLISTAS_LARGADA);
     } else {
-        posicao_pista = n / MAX_CICLISTAS_LARGADA - 1;
+        i = n / MAX_CICLISTAS_LARGADA - 1;
     }
 
     j = 0;
-    while (i > 0) {
+    while (n_ciclistas > 0) {
         id_ciclista = rand() % n;
 
         /**
@@ -223,10 +297,12 @@ void dar_largada(posicao_t*** pista, int d, int n) {
          */
         if (!ids[id_ciclista]) {
             ids[id_ciclista] = TRUE;
-            info_ciclista = init_ciclista(id_ciclista, i);
-            pista[posicao_pista][j]->ciclista = info_ciclista;
+            ciclista = init_ciclista(id_ciclista, i, j);
 
-            i--;
+            simulacao->pista[i][j]->ciclista = ciclista;
+            simulacao->ciclistas[n - n_ciclistas] = ciclista;
+
+            n_ciclistas--;
             j++;
 
             /**
@@ -234,7 +310,7 @@ void dar_largada(posicao_t*** pista, int d, int n) {
              */
             if (j == MAX_CICLISTAS_LARGADA) {
                 j = 0;
-                posicao_pista--;
+                i--;
             }
         }
     }
@@ -242,26 +318,8 @@ void dar_largada(posicao_t*** pista, int d, int n) {
 }
 
 
-void print_pista(posicao_t*** pista, int d) {
-    int i, j;
-
-    for (i = 0; i < d; i++) {
-        fprintf(stderr, "pos %2d: \t", (i + 1));
-        for (j = 0; j < MAX_CICLISTAS; j++) {
-            if (pista[i][j] != NULL && pista[i][j]->ciclista != NULL) {
-                fprintf(stderr, "%02d \t", pista[i][j]->ciclista->velocidade);
-            } else {
-                fprintf(stderr, "  -  \t");
-            }
-        }
-        fprintf(stderr, "\n");
-    }
-}
-
-
 int main(int argc, char* argv[]) {
     int n, d;
-    simulacao_t* simulacao;
     
     if (argc < 3) {
         fprintf(stderr, "Uso: ./ep2 <d> <n> [debug]\n");
@@ -277,6 +335,22 @@ int main(int argc, char* argv[]) {
 
     simulacao = init_simulacao(d, n);
     dar_largada(simulacao->pista, d, n);
+    
+    print_ciclistas();
+
+    // pthread_create(&simulacao->ciclistas[0]->thread, NULL, simular_ciclista, simulacao->ciclistas[0]);
+    for (int i = 0; i < 20; i++) {
+        print_pista();
+        usleep(INTERVAL_60MS);
+    }
+    // pthread_join(simulacao->ciclistas[0]->thread, NULL);
+
+    /**
+     * Aguarda até que todas as threads tenha finalizado sua simulação.
+     */
+    for (int i = 0; i < n; i++) {
+        pthread_join(simulacao->ciclistas[i]->thread, NULL);
+    }
 
     free_simulacao(simulacao, d);
 
