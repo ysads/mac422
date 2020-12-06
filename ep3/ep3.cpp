4779ecef3d5769f17bba9c79b617c634091628a5
Ygor Sad <ysads2010@gmail.com>
2020-12-06 18:36:57 -0300

    feat: add mkdir

--- ep3/ep3.cpp
+++ ep3/ep3.cpp
@@ -32,14 +32,19 @@
 #define FAT_SIZE (MAX_BLOCKS * sizeof(int))
 
 /**
- * Posição dentro do FS a partir da qual podem ser escritos arquivos e diretório/
+ * Posição dentro do FS a partir da qual podem ser escritos arquivos e diretórios
  */
 #define INITIAL_OFFSET (BITMAP_SIZE + FAT_SIZE + BLOCK_SIZE)
 
 /**
+ * Representa o primeiro bloco disponível para uso no sistema.
+ */
+#define INITIAL_BLOCK (INITIAL_OFFSET / BLOCK_SIZE)
+
+/**
  * Tamanho máximo que um nome de arquivo pode ter.
  */
-#define FILENAME_SIZE 124
+#define FILENAME_SIZE 120
 
 /**
  * O tamanho máximo dos metadados de um arquivo.
@@ -52,8 +57,8 @@
 #define MAX_CHILDREN_PER_DIR (BLOCK_SIZE / CHILD_SIZE)
 
 
-
 #define debug(k, v) cout << k << ": " << v << endl;
+#define fail(msg) cerr << "=> ERRO: " << msg << endl;
 
 using namespace std;
 
@@ -63,6 +68,7 @@ typedef struct {
   time_t last_access;
   time_t last_modified;
   size_t size;
+  int is_dir;
   int head;
 } vattr_t;
 
@@ -123,6 +129,29 @@ void print_cmd(cmd_t command) {
 }
 
 
+void print_attr(vattr_t attrs) {
+  printf("------------------\n");
+  printf("%s (%zu bytes)\n",  attrs.name.c_str(), attrs.size);
+  printf("|> #%d\n", attrs.head);
+  printf("|> create: %ld\n", attrs.created);
+  printf("|> access: %ld\n", attrs.last_access);
+  printf("|> modify: %ld\n", attrs.last_modified);
+  printf("|> null? %d\n", (attrs.created == NULL));
+  printf("|> dir? %d\n", (attrs.size == -1));
+}
+
+void print_dir(vdir_t dir) {
+  if (dir.children.size() == MAX_CHILDREN_PER_DIR) {
+    printf("----------------\n");
+    printf("|> empty dir!\n");
+  } else {
+    for (auto child: dir.children) {
+      print_attr(child);
+    }
+  }
+}
+
+
 /**
  * Diz se o arquivo lido está vazio comparando o número de bytes da atual posição.
  * OBS: depende de a posição atual corresponder ao final do arquivo.
@@ -171,14 +200,20 @@ void write_fat_to_fs() {
  * Escreve os metadados de um diretório na posição dada.
  */
 void write_dir_to_fs(int position, vdir_t content) {
-  fs.file.seekp(position);
+  fs.file.seekp(position, ios::beg);
+
+  // debug("nous some", position);
+  debug("block", position / BLOCK_SIZE);
+  debug("will write children", content.children.size());
 
   for (auto child : content.children) {
+    debug("escrevendo", child.name);
     fs.file.write((char*) &child.created, sizeof(time_t));
     fs.file.write((char*) &child.last_access, sizeof(time_t));
     fs.file.write((char*) &child.last_modified, sizeof(time_t));
-    fs.file.write(reinterpret_cast<const char *>(&child.size), sizeof(size_t));
-    fs.file.write(reinterpret_cast<const char *>(&child.head), sizeof(int));
+    fs.file.write(reinterpret_cast<const char*>(&child.size), sizeof(size_t));
+    fs.file.write(reinterpret_cast<const char*>(&child.head), sizeof(int));
+    fs.file.write(reinterpret_cast<const char*>(&child.is_dir), sizeof(int));
     fs.file.write(child.name.c_str(), FILENAME_SIZE);
   }
 
@@ -191,6 +226,7 @@ void write_dir_to_fs(int position, vdir_t content) {
     time_t null_time = NULL;
     fs.file.write((char*) &null_time, sizeof(time_t));
   }
+  // fs.file.flush();
 }
 
 
@@ -198,8 +234,8 @@ void write_dir_to_fs(int position, vdir_t content) {
  * Inicializa os metadados do diretório '/', armazenando-os no bloco
  * imediatamente após o último bloco ocupado pela tabela FAT.
  */
-void initialize_empty_root() {
-  vdir_t root_dir_parent;
+void init_empty_root() {
+  vdir_t root_dir, root_dir_parent;
   vattr_t root_attrs;
   time_t now;
   int root_pos;
@@ -213,8 +249,9 @@ void initialize_empty_root() {
   root_attrs.created = now;
   root_attrs.last_access = now;
   root_attrs.last_modified = now;
-  root_attrs.size = -1;
+  root_attrs.size = 0;
   root_attrs.head = root_pos + 1;
+  root_attrs.is_dir = 1;
   root_attrs.name = "/";
 
   /**
@@ -228,7 +265,11 @@ void initialize_empty_root() {
   fs.bitmap[root_pos+1] = '0';
   fs.fat[root_pos+1] = -1;
 
+  debug("usando bloco", root_pos);
+  debug("usando bloco", root_pos+1);
+
   write_dir_to_fs(root_pos * BLOCK_SIZE, root_dir_parent);
+  write_dir_to_fs((root_pos + 1) * BLOCK_SIZE, root_dir);
 }
 
 
@@ -237,17 +278,14 @@ void initialize_empty_root() {
  * um bitmap vazio, uma quase–vazia tabela fat e os metadados do diretório /
  */
 void init_empty_fs() {
-  int k;
-  // marca como 0 os primeiros blocos ocupados do disco – / e seus metadados
   for (int i = 0; i < MAX_BLOCKS; i++) {
-    k = (i % 3 == 0) ? 1 : 0;
-    fs.bitmap[i] = k + '0';
-    fs.fat[i] = i * k;
+    fs.bitmap[i] = '1';
+    fs.fat[i] = 0;
   }
 
   write_bitmap_to_fs();
   write_fat_to_fs();
-  initialize_empty_root();
+  init_empty_root();
 }
 
 
@@ -260,6 +298,50 @@ void parse_fs() {
 
   fs.file.seekg(BITMAP_SIZE, ios::beg);
   fs.file.read((char*) fs.fat, MAX_BLOCKS * sizeof(int));
+}
+
+
+/**
+ * Interpreta o diretório contido na posição especificada retornando-o.
+ */
+vdir_t parse_dir(int position) {
+  vdir_t dir;
+  char name[FILENAME_SIZE];
+  int block;
+
+  /**
+   * Verifica se esse é um bloco livre. Se for, não há o que parsear.
+   */
+  block = position / BLOCK_SIZE;
+  if (fs.bitmap[block] == '1') {
+    // printf("vazio: bloco %d\n", block);
+    return dir;
+  }
+  // printf("\ndir @ #%d (byte %d)...\n", position);
+
+  fs.file.seekg(ios::beg);
+  fs.file.seekg(position);
+
+  do {
+    vattr_t attrs;
+
+    fs.file.read((char*) &attrs.created, sizeof(time_t));
+    if (attrs.created == NULL) break;
+
+    fs.file.read((char*) &attrs.last_access, sizeof(time_t));
+    fs.file.read((char*) &attrs.last_modified, sizeof(time_t));
+    fs.file.read(reinterpret_cast<char*>(&attrs.size), sizeof(size_t));
+    fs.file.read(reinterpret_cast<char*>(&attrs.head), sizeof(int));
+    fs.file.read(reinterpret_cast<char*>(&attrs.is_dir), sizeof(int));
+    fs.file.read(name, FILENAME_SIZE);
+
+    attrs.name = name;
+    dir.children.push_back(attrs);
+
+    print_attr(attrs);
+  } while (dir.children.size() < MAX_CHILDREN_PER_DIR);
+
+  return dir;
 }
 
 
@@ -299,20 +381,6 @@ void unmount(cmd_t command) {
 
   fs.file.close();
 }
-
-
-// void mkdir (cmd_t command) {
-//   vdir_t* diretorio;
-
-//   diretorio=malloc(sizeof(vdir_t*));
-
-//   diretorio->name =command->argv[0];
-//   for(i=1;command->args[i]!=null;i++){
-//     diretorio->name+= " ";
-//     diretorio->name+= command->args[i];
-//   }
-//   /*colocar no ARQUIVO */
-// }
 
 
 // void rmdir (cmd_t command) {
@@ -380,23 +448,126 @@ void unmount(cmd_t command) {
 // }
 
 
+/**
+ * Busca linearmente em todo o filesystem pelo primeiro block vazio disponível.
+ * @throw quando não há espaço disponível.
+ */
+int find_empty_block() {
+  for (int i = INITIAL_BLOCK; i < MAX_BLOCKS; i++) {
+    // printf("#%d? %c\n", i, fs.bitmap[i]);
+    if (fs.bitmap[i] == '1') {
+      return i;
+    }
+  }
+  throw "Não há espaço disponível em disco!";
+}
+
+
+/**
+ * Cria um novo arquivo vazio no filesystem, usando o primeiro bloco disponível.
+ */
+vattr_t init_file(string filename, int is_dir) {
+  vattr_t new_file;
+  time_t now;
+  int head_block;
+
+  now = time(nullptr);
+  head_block = find_empty_block();
+
+  new_file.created = now;
+  new_file.last_access = now;
+  new_file.last_modified = now;
+  new_file.name = filename;
+  new_file.size = 0;
+  new_file.head = head_block;
+  new_file.is_dir = is_dir;
+
+  /**
+   * Marca como usado, no filesystem, o espaço do arquivo recém-criado.
+   */
+  fs.bitmap[head_block] = '0';
+  fs.fat[head_block] = -1;
+
+  return new_file;
+}
+
+
+/**
+ * Adiciona no diretório um novo arquivo vazio, usando o nome passado.
+ * @throw quando o diretório já está cheio.
+ */
+vdir_t insert_file_into_dir(vdir_t dir, string child_name, int is_dir) {
+  vattr_t file;
+
+  if (dir.children.size() == MAX_CHILDREN_PER_DIR) {
+    throw "Diretório cheio. Não é possível adicionar conteúdo aqui!";
+  }
+
+  /**
+   * Inicializa um arquivo vazio e o adiciona ao diretório.
+   */
+  file = init_file(child_name, is_dir);
+  dir.children.push_back(file);
+
+  return dir;
+}
+
+
+void mkdir(cmd_t command) {
+  string dir_name;
+  streampos dir_position;
+  vdir_t dir;
+  vdir_t new_dir;
+
+  dir_name = command.args[0];
+  dir_position = INITIAL_OFFSET;
+  dir = parse_dir(dir_position);
+
+  for (int i = 0; i < dir.children.size(); i++) {
+    /**
+     * Localizamos o arquivo nesse diretório. Atualiza o last_access e escreve
+     * o diretório de volta no filesystem.
+     */
+    if (dir.children[i].name == dir_name) {
+      throw ("Não é possível criar diretório. " +  dir_name + " já existe!").c_str();
+    }
+  }
+
+  new_dir = insert_file_into_dir(dir, dir_name, 1);
+  print_dir(new_dir);
+  write_dir_to_fs(dir_position, new_dir);
+}
+
+
 void touch(cmd_t command) {
-  // vfile_t* arquivo;
+  string searched_file;
+  vdir_t dir;
+  int dir_position;
 
-  // /*procurar arquivo*/
+  searched_file = command.args[0];
+  dir_position = INITIAL_OFFSET;
+  dir = parse_dir(dir_position);
 
-  // if(/*achou arquivo na procura*/) time(&arquivo->last_access);
-  // else{
-  //   arquivo=malloc(sizeof(vfile_t));
-  //   arquivo->name = command->args[0];
-  //   for(i=1;arquivo->args[i]!=null;i++){
-  //     arquivo->name+= " ";
-  //     arquivo->name+= command->args[i];
+  // for (int i = 0; i < dir.children.size(); i++) {
+  //   /**
+  //    * Localizamos o arquivo nesse diretório. Atualiza o last_access e escreve
+  //    * o diretório de volta no filesystem.
+  //    */
+  //   if (dir.children[i].name == searched_file) {
+  //     dir.children[i].last_access = time(nullptr);
+  //     write_dir_to_fs(dir_position, dir);
+  //     return;
   //   }
-
-  //   arquivo->size=?
-  //   time(&arquivo->created)
   // }
+
+  // printf("parse: [ %lu children ]\n", dir.children.size());
+
+  dir = insert_file_into_dir(dir, searched_file, 0);
+  write_dir_to_fs(dir_position, dir);
+
+  // printf("after: [ %lu children ]\n\n", dir.children.size());
+
+  // print_dir(dir);
 }
 
 
@@ -473,51 +644,57 @@ cmd_t prompt_command() {
 
 
 void hook(cmd_t command) {
-  vattr_t attrs;
+  vdir_t dir;
   char name[FILENAME_SIZE];
   int block = stoi(command.args[0]);
   int pos = block * BLOCK_SIZE;
-  int children = 0;
 
-  printf("\nhooking into #%d (byte %d)...\n", block, pos);
-  printf("bitmap: %c – fat: %d\n", fs.bitmap[block], fs.fat[block]);
-
+  fs.file.seekg(ios::beg);
   fs.file.seekg(pos);
 
+  printf("\nhooking into #%d (byte %d)...\n", block, pos);
+  // printf("@ %zu byte\n", fs.file.tellg());
+  // printf("bitmap: %c – fat: %d\n", fs.bitmap[block], fs.fat[block]);
+
   do {
+    vattr_t attrs;
+
     fs.file.read((char*) &attrs.created, sizeof(time_t));
+    if (attrs.created == NULL) break;
+
     fs.file.read((char*) &attrs.last_access, sizeof(time_t));
     fs.file.read((char*) &attrs.last_modified, sizeof(time_t));
     fs.file.read(reinterpret_cast<char*>(&attrs.size), sizeof(size_t));
     fs.file.read(reinterpret_cast<char*>(&attrs.head), sizeof(int));
+    fs.file.read(reinterpret_cast<char*>(&attrs.is_dir), sizeof(int));
     fs.file.read(name, FILENAME_SIZE);
     attrs.name = name;
 
-    if (attrs.created == NULL) break;
-    children++;
+    dir.children.push_back(attrs);
 
-    printf("------------------\n");
-    printf("|> #%d %s (%zu bytes)\n", attrs.head, attrs.name.c_str(), attrs.size);
-    printf("|> create: %ld\n", attrs.created);
-    printf("|> access: %ld\n", attrs.last_access);
-    printf("|> modify: %ld\n", attrs.last_modified);
-    printf("|> null? %d\n", (attrs.created == NULL));
-    printf("|> dir? %d\n", (attrs.size == -1));
-  } while (children < MAX_CHILDREN_PER_DIR);
+  } while (dir.children.size() < MAX_CHILDREN_PER_DIR);
 
-  printf("\n[ %d children read ]\n", children);
+  print_dir(dir);
+
+  printf("\n[ %lu children read ]\n\n", dir.children.size());
 }
 
 
 void execute(cmd_t command) {
-  if (command.cmd == "mount") {
-    mount(command);
-  } else if (command.cmd == "unmount") {
-    unmount(command);
-  } else if (command.cmd == "touch") {
-    touch(command);
-  } else if (command.cmd == "hook") {
-    hook(command);
+  try {
+    if (command.cmd == "mount") {
+      mount(command);
+    } else if (command.cmd == "unmount") {
+      unmount(command);
+    } else if (command.cmd == "touch") {
+      touch(command);
+    } else if (command.cmd == "mkdir") {
+      mkdir(command);
+    } else if (command.cmd == "hook") {
+      hook(command);
+    }
+  } catch (const char* msg) {
+    fail(msg);
   }
 }
 
